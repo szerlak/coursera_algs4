094cfb67bb818cef4d5a8725bad1df6ea7714af6
Tomasz <szerletomasz@gmail.com>
2014-07-30 00:59:30 +0200

    Prettify code

--- Alg1_5_Kd-Trees/src/PointSET.java
+++ Alg1_5_Kd-Trees/src/PointSET.java
@@ -1,7 +1,6 @@
 import java.util.LinkedList;
 import java.util.List;
 
-
 /**
  * 
  * @author Tomasz Szerle
@@ -10,93 +9,93 @@ import java.util.List;
  */
 public class PointSET {
 
-	private SET<Point2D> points;
-	
-	/**
-	 * construct an empty set of points
-	 */
-	public PointSET() {
-		points = new SET<Point2D>();
-	}
+    private SET<Point2D> points;
 
-	/**
-	 * is the set empty?
-	 * 
-	 * @return
-	 */
-	public boolean isEmpty() {
-		return points.isEmpty();
-	}
+    /**
+     * construct an empty set of points
+     */
+    public PointSET() {
+        points = new SET<Point2D>();
+    }
 
-	/**
-	 * number of points in the set
-	 * 
-	 * @return
-	 */
-	public int size() {
-		return points.size();
-	}
+    /**
+     * is the set empty?
+     * 
+     * @return
+     */
+    public boolean isEmpty() {
+        return points.isEmpty();
+    }
 
-	/**
-	 * add the point p to the set (if it is not already in the set)
-	 * 
-	 * @param p
-	 */
-	public void insert(Point2D p) {
-		points.add(p);
-	}
+    /**
+     * number of points in the set
+     * 
+     * @return
+     */
+    public int size() {
+        return points.size();
+    }
 
-	/**
-	 * does the set contain the point p?
-	 * 
-	 * @param p
-	 * @return
-	 */
-	public boolean contains(Point2D p) {
-		return points.contains(p);
-	}
+    /**
+     * add the point p to the set (if it is not already in the set)
+     * 
+     * @param p
+     */
+    public void insert(Point2D p) {
+        points.add(p);
+    }
 
-	/**
-	 * draw all of the points to standard draw
-	 */
-	public void draw() {
-		for (Point2D point : points) {
-			point.draw();
-		}
-	}
+    /**
+     * does the set contain the point p?
+     * 
+     * @param p
+     * @return
+     */
+    public boolean contains(Point2D p) {
+        return points.contains(p);
+    }
 
-	/**
-	 * all points in the set that are inside the rectangle
-	 * 
-	 * @param rect
-	 * @return
-	 */
-	public Iterable<Point2D> range(RectHV rect) {
-		List<Point2D> inRange = new LinkedList<Point2D>();
-		for (Point2D point : points) {
-			if (rect.contains(point))
-				inRange.add(point);
-		}
-		return inRange;
-	}
+    /**
+     * draw all of the points to standard draw
+     */
+    public void draw() {
+        for (Point2D point : points) {
+            point.draw();
+        }
+    }
 
-	/**
-	 * a nearest neighbor in the set to p; null if set is empty
-	 * 
-	 * @param p
-	 * @return
-	 */
-	public Point2D nearest(Point2D p) {
-		Point2D minPoint = null;
-		double minDistance = Double.POSITIVE_INFINITY;
-		
-		for (Point2D point : points) {
-			double distance = p.distanceTo(point);
-			if (distance < minDistance) {
-				minDistance = distance;
-				minPoint = point;
-			}
-		}
-		return minPoint;
-	}
+    /**
+     * all points in the set that are inside the rectangle
+     * 
+     * @param rect
+     * @return
+     */
+    public Iterable<Point2D> range(RectHV rect) {
+        List<Point2D> inRange = new LinkedList<Point2D>();
+        for (Point2D point : points) {
+            if (rect.contains(point))
+                inRange.add(point);
+        }
+        return inRange;
+    }
+
+    /**
+     * a nearest neighbor in the set to p; null if set is empty
+     * 
+     * @param p
+     * @return
+     */
+    public Point2D nearest(Point2D p) {
+        Point2D minPoint = null;
+        double minDistance = Double.POSITIVE_INFINITY;
+
+        for (Point2D point : points) {
+            double distance = p.distanceTo(point);
+            if (distance < minDistance) {
+                minDistance = distance;
+                minPoint = point;
+            }
+        }
+        return minPoint;
+    }
 }
