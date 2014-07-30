fe2951251d826d16969f2d311352381b1c158d02
Tomasz <szerletomasz@gmail.com>
2014-07-30 18:39:06 +0200

    [Alg1_5_KdTree] Alternative solution - 100% score

--- Alg1_5_Kd-Trees/src/KdTree.java
+++ Alg1_5_Kd-Trees/src/KdTree.java
@@ -13,7 +13,6 @@ public class KdTree {
         private Node left;
         private Node right;
         private Point2D point;
-        private RectHV rectangle;
         private boolean vertical;
 
         // public Node(Point2D p, boolean v, RectHV rect, Node l, Node r) {
@@ -24,10 +23,9 @@ public class KdTree {
         // vertical = v;
         // }
 
-        public Node(Point2D p, boolean v, RectHV rect) {
+        public Node(Point2D p, boolean v) {
             left = null;
             right = null;
-            rectangle = rect;
             point = p;
             vertical = v;
         }
@@ -70,36 +68,7 @@ public class KdTree {
      * @param p
      */
     public void insert(Point2D p) {
-        root = insertRecursive(root, p, true, CONTAINER);
-    }
-
-    private Node insertRecursive(Node node, Point2D p, boolean vertical, RectHV rect) {
-        if (node == null) {
-            size++;
-            return new Node(p, vertical, rect);
-        }
-        if (node.point.equals(p))
-            return node;
-
-        if (node.vertical) {
-            if (p.x() < node.point.x()) {
-                RectHV left = new RectHV(rect.xmin(), rect.ymin(), node.point.x(), rect.ymax());
-                node.left = insertRecursive(node.left, p, !node.vertical, left);
-            } else {
-                RectHV right = new RectHV(node.point.x(), rect.ymin(), rect.xmax(), rect.ymax());
-                node.right = insertRecursive(node.right, p, !node.vertical, right);
-
-            }
-        } else {
-            if (p.y() < node.point.y()) {
-                RectHV down = new RectHV(rect.xmin(), rect.ymin(), rect.xmax(), node.point.y());
-                node.left = insertRecursive(node.left, p, !node.vertical, down);
-            } else {
-                RectHV up = new RectHV(rect.xmin(), node.point.y(), rect.xmax(), rect.ymax());
-                node.right = insertRecursive(node.right, p, !node.vertical, up);
-            }
-        }
-        return node;
+        root = insertRecursive(root, p, true);
     }
 
     /**
@@ -112,6 +81,61 @@ public class KdTree {
         return containsRecursive(root, p);
     }
 
+    /**
+     * draw all of the points to standard draw
+     */
+    public void draw() {
+        StdDraw.setScale(0, 1);
+        drawRecursive(root, CONTAINER);
+    }
+
+    /**
+     * all points in the set that are inside the rectangle
+     * 
+     * @param rect
+     * @return
+     */
+    public Iterable<Point2D> range(RectHV rect) {
+        List<Point2D> inRange = new LinkedList<Point2D>();
+        rangeRecursive(root, CONTAINER, rect, inRange);
+        return inRange;
+    }
+
+    /**
+     * a nearest neighbor in the tree to p; null if tree is empty
+     * 
+     * @param p
+     * @return
+     */
+    public Point2D nearest(Point2D p) {
+        return nearestRecursive(root, CONTAINER, p, null);
+    }
+
+    private Node insertRecursive(Node node, Point2D p, boolean vertical) {
+        if (node == null) {
+            size++;
+            return new Node(p, vertical);
+        }
+        if (node.point.equals(p))
+            return node;
+
+        if (node.vertical) {
+            if (p.x() < node.point.x()) {
+                node.left = insertRecursive(node.left, p, false);
+            } else {
+                node.right = insertRecursive(node.right, p, false);
+
+            }
+        } else {
+            if (p.y() < node.point.y()) {
+                node.left = insertRecursive(node.left, p, true);
+            } else {
+                node.right = insertRecursive(node.right, p, true);
+            }
+        }
+        return node;
+    }
+    
     private boolean containsRecursive(Node node, Point2D p) {
         if (node == null)
             return false;
@@ -125,15 +149,7 @@ public class KdTree {
             return containsRecursive(node.right, p);
     }
 
-    /**
-     * draw all of the points to standard draw
-     */
-    public void draw() {
-        StdDraw.setScale(0, 1);
-        drawRecursive(root);
-    }
-
-    private void drawRecursive(Node node) {
+    private void drawRecursive(Node node, RectHV rect) {
         if (node == null)
             return;
 
@@ -144,56 +160,33 @@ public class KdTree {
         Point2D min, max;
         if (node.vertical) {
             StdDraw.setPenColor(StdDraw.RED);
-            min = new Point2D(node.point.x(), node.rectangle.ymin());
-            max = new Point2D(node.point.x(), node.rectangle.ymax());
+            min = new Point2D(node.point.x(), rect.ymin());
+            max = new Point2D(node.point.x(), rect.ymax());
         } else {
             StdDraw.setPenColor(StdDraw.BLUE);
-            min = new Point2D(node.rectangle.xmin(), node.point.y());
-            max = new Point2D(node.rectangle.xmax(), node.point.y());
+            min = new Point2D(rect.xmin(), node.point.y());
+            max = new Point2D(rect.xmax(), node.point.y());
         }
-        StdDraw.setPenColor(StdDraw.RED);
         StdDraw.setPenRadius();
         min.drawTo(max);
 
-        drawRecursive(node.left);
-        drawRecursive(node.right);
+        drawRecursive(node.left, getRect(true, node, rect));
+        drawRecursive(node.right, getRect(false, node, rect));
     }
 
-    /**
-     * all points in the set that are inside the rectangle
-     * 
-     * @param rect
-     * @return
-     */
-    public Iterable<Point2D> range(RectHV rect) {
-        List<Point2D> inRange = new LinkedList<Point2D>();
-        rangeRecursive(root, rect, inRange);
-        return inRange;
-    }
-
-    private void rangeRecursive(Node node, final RectHV rect, final List<Point2D> inRange) {
+    private void rangeRecursive(Node node, RectHV nodeRect, final RectHV rect, final List<Point2D> inRange) {
         if (node == null)
             return;
 
-        if (rect.intersects(node.rectangle)) {
+        if (rect.intersects(nodeRect)) {
             if (rect.contains(node.point))
                 inRange.add(node.point);
-            rangeRecursive(node.left, rect, inRange);
-            rangeRecursive(node.right, rect, inRange);
+            rangeRecursive(node.left, getRect(true, node, nodeRect), rect, inRange);
+            rangeRecursive(node.right, getRect(false, node, nodeRect), rect, inRange);
         }
     }
 
-    /**
-     * a nearest neighbor in the tree to p; null if tree is empty
-     * 
-     * @param p
-     * @return
-     */
-    public Point2D nearest(Point2D p) {
-        return nearestRecursive(root, p, null);
-    }
-
-    private Point2D nearestRecursive(Node node, Point2D p, final Point2D currentNearest) {
+    private Point2D nearestRecursive(Node node, RectHV nodeRect, Point2D p, final Point2D currentNearest) {
         if (node == null)
             return currentNearest;
 
@@ -203,7 +196,7 @@ public class KdTree {
 
         if (nearest != null) {
             distFromCurrentPoint = p.distanceSquaredTo(nearest);
-            distFromNewRect = node.rectangle.distanceSquaredTo(p);
+            distFromNewRect = nodeRect.distanceSquaredTo(p);
         }
 
         if (nearest == null || distFromCurrentPoint > distFromNewRect) {
@@ -213,13 +206,30 @@ public class KdTree {
 
             if ((node.vertical && p.x() < node.point.x())
                     || (!node.vertical && p.y() < node.point.y())) {
-                nearest = nearestRecursive(node.left, p, nearest);
-                nearest = nearestRecursive(node.right, p, nearest);
+                nearest = nearestRecursive(node.left, getRect(true, node, nodeRect), p, nearest);
+                nearest = nearestRecursive(node.right, getRect(false, node, nodeRect), p, nearest);
             } else {
-                nearest = nearestRecursive(node.right, p, nearest);
-                nearest = nearestRecursive(node.left, p, nearest);
+                nearest = nearestRecursive(node.right, getRect(false, node, nodeRect), p, nearest);
+                nearest = nearestRecursive(node.left, getRect(true, node, nodeRect), p, nearest);
             }
         }
         return nearest;
+    }
+
+    private RectHV getRect(boolean left, Node node, RectHV rect) {
+        RectHV ret = null;
+        if (node.vertical) {
+            if (left)
+                ret = new RectHV(rect.xmin(), rect.ymin(), node.point.x(), rect.ymax());
+            else
+                ret = new RectHV(node.point.x(), rect.ymin(), rect.xmax(), rect.ymax());
+        } else {
+            if (left)
+                ret = new RectHV(rect.xmin(), rect.ymin(), rect.xmax(), node.point.y());
+            else
+                ret = new RectHV(rect.xmin(), node.point.y(), rect.xmax(), rect.ymax());
+        }
+        
+        return ret;
     }
 }
